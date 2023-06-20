114a3fd16658e546cbd31cc598e9dbd4582b647a
loeeeee <95266635+loeeeee@users.noreply.github.com>
2023-06-20 15:26:23 +0800

    create quick setup script

---
+++ quick_setup_gpu.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+working_dir = $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$working_dir"
+
+echo "Install python"
+apt install python3.11-venv
+apt install python3-pip
+apt install python-is-python3
+
+echo "Installing venv"
+pip install virtualenv
+python3 -m venv .env
+
+echo "Starting python environment"
+source .env/bin/activate
+
+echo "Installing pytorch"
+pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121
+
+echo "Installing package according to requirement"
+cat requirements_gpu.txt | xargs -n 1 pip install
+
+mkdir src
+mkdir data
+mkdir data/processed_norm
