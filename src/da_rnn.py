0dfb5beb4635907384a75777c0497e4dc88ad9d5
loeeeee <95266635+loeeeee@users.noreply.github.com>
2023-07-07 10:07:06 +0800

    Updating naming scheme

--- src/da_rnn.py
+++ src/da_rnn.py
@@ -1,6 +1,10 @@
 import torch
 from torch import nn
 
+"""
+TODO: Hahahaha XD
+"""
+
 class DualStageAttentionBasedRecurrentNeuralNetwork(nn.Module):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
